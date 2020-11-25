0efb4c76b12030ba4301bcbde2654b46162c1b52
Michal Lehuta <66588074+MichalLeh@users.noreply.github.com>
2020-11-25 12:47:55 +0100

    Add files via upload
    Engeto SQL projekt verze 2.0

--- Engeto SQL projekt.sql
+++ Engeto SQL projekt.sql
@@ -1,40 +1,64 @@
-create table t_michal_lehuta_projekt_SQL_final as
-select ctr.country, ctr.population_density, ctr.median_age_2018, ec.GDP_per_head, ec2.mortaliy_under5, ec3.gini, 
+# COUNTRIES TABLE
+Create table t_michal_lehuta_SQLprojekt_Countries as
+select ctr.country, ec.population, ec.GDP_per_head, ctr.population_density, ctr.median_age_2018, ec2.mortaliy_under5, ec3.gini, 
 # life_exp_diff - rozdíl mezi oèekávanou dobou dožití v roce 1965 a v roce 2015
-(le.life_exp2015-le2.life_exp1965) as life_exp_diff, 
-# religion_ratio - podíl jednotlivých náboženství na celkovém obyvatelstvu
-rel.religion, round(rel.population/rel2.total_population*100, 2) as religion_ratio, 
-w1.city, w1.date, w2.binary_day, w2.season_code, w2.daily_avg_temp, w3.count_zero_rain, w4.max_day_wind
+(le.life_exp2015-le2.life_exp1965) as life_exp_diff
 from 
-(select  country, population_density, median_age_2018, capital_city from countries) ctr 
-left join
-# GDP_per_head - HDP na obyvatele
-(select country, max(year) as max_year, round(GDP/population, 2) as GDP_per_head from economies where GDP != 'Null' group by country) ec 
+(select country, population_density, median_age_2018, capital_city from countries) ctr 
+join
+# GDP_per_head - HDP na obyvatele; inner join nezahrnuje pouze stát Sýrii
+(select country, population, round(GDP/population, 2) as GDP_per_head from economies where GDP is not Null group by country) ec 
 on ctr.country = ec.country
-left join 
+join 
 # dìtská úmrtnost
-(select country, mortaliy_under5, max(year) from economies e2 where mortaliy_under5 != 'Null' group by country) ec2
+(select country, mortaliy_under5, max(year) from economies e2 where mortaliy_under5 is not Null group by country) ec2
 on ctr.country = ec2.country
 left join 
-# Gini koeficient
-(select country, gini, max(year) as max_year_2 from economies where gini != 'Null' group by country) ec3
+# Gini koeficient; inner join vynechá státy jako: Afghanistan, New Zealand, Saudi Arabia, Cuba, Singapore, Libya atd... a zároveò nemají Null hodnotu v ostatních zkoumaných promìnných
+(select country, gini, max(year) from economies where gini is not Null group by country) ec3
 on ctr.country = ec3.country
+# left join zahrnuje rovnìž Russian Federation (144 mil. obyvatel)
 left join 
 (select country, life_expectancy as life_exp2015 from life_expectancy where year = 2015) le
 on ctr.country = le.country 
- left join 
+join 
 (select country, life_expectancy as life_exp1965 from life_expectancy where year = 1965) le2 
-on le.country = le2.country
-left join 
-(select country, religion, population from religions where year = 2020) rel 
-on ctr.country = rel.country
-left join 
-(select country, sum(population) as total_population from religions where year = 2020 group by country) rel2 
-on rel.country = rel2.country
-left join
+on le.country = le2.country order by ec3.gini desc
+
+# RELIGION TABLE - podíl jednotlivých náboženství na celkovém obyvatelstvu
+Create table t_michal_lehuta_SQLprojekt_Religions as
+select rbase.country, rbase.population, 
+round(rbase.Christianity/r1.total_population*100, 2) as Christianity, round(rbase.Islam/r1.total_population*100, 2) as Islam, round(rbase.Unaffiliated_religions/r1.total_population*100, 2) as Unaffiliated_religions,
+round(rbase.Hinduism/r1.total_population*100, 2) as Hinduism, round(rbase.Buddhism/r1.total_population*100, 2) as Buddhism, round(rbase.Folk_religions/r1.total_population*100, 2) as Folk_religions,
+round(rbase.Other_religions/r1.total_population*100, 2) as Other_religions, round(rbase.Judaism/r1.total_population*100, 2) as Judaism
+from 
+(select country, sum(population) as population, 
+sum(case when religion = 'Christianity' then population else 0 end) as Christianity,
+sum(case when religion = 'Islam' then population else 0 end) as Islam,
+sum(case when religion = 'Unaffiliated Religions' then population else 0 end) as Unaffiliated_religions, 
+sum(case when religion = 'Hinduism' then population else 0 end) as Hinduism, 
+sum(case when religion = 'Buddhism' then population else 0 end) as Buddhism, 
+sum(case when religion = 'Folk Religions' then population else 0 end) as Folk_religions,
+sum(case when religion = 'Other Religions' then population else 0 end) as Other_religions,
+sum(case when religion = 'Judaism' then population else 0 end) as Judaism
+from religions r where year = 2020 group by country) rbase 
+JOIN 
+(select country, sum(population) as total_population from religions r2 where year = 2020 group by country) r1 
+on rbase.country = r1.country
+
+# COVID TABLE
+Create table t_michal_lehuta_SQLprojekt_Covid19 as
+select cbd.date, cbd.country, cbd.confirmed, ct.tests_performed from 
+(select date, country, confirmed from covid19_basic_differences group by date, country) cbd 
+join
+(select country, date, tests_performed from covid19_tests group by date, country) ct
+on cbd.country = ct.country and cbd.date = ct.date 
+
+# WEATHER TABLE
+Create table t_michal_lehuta_SQLprojekt_Weather as
+select w1.city, w1.date, w2.binary_day, w2.season_code, w2.daily_avg_temp, w3.count_zero_rain, w4.max_day_wind from 
 (select city, date from weather group by city, date) w1
-on w1.city = ctr.capital_city
-left join 
+left join
 (select city, date,
 # binární promìnná pro víkend = 1, pracovní den = 0
 case when dayname(date) in ('Sunday', 'Saturday') then 1 else 0 end as binary_day, 
